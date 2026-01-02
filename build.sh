1f01951ac45d19f31b066202ef0db7313d703304
David Hoppenbrouwers <lemmings@demindiro.com>
2026-01-02 18:38:28 +0100

    move x64 code to subdir

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-out=./build
 set -xe
-mkdir -p $out/root
-fasm bootloader.asm $out/kernel
-fasm init/hello.asm $out/init
-echo "Hello, world! This is an auxiliary file" > $out/aux
-fasm gsboot.asm $out/root.gsboot
-fasm mbr.asm $out/root.img
+export out=./build
+exec sh x64/build.sh
