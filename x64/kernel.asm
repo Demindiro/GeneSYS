3983300074867e874c2b9278e746ed3599d6ab96
David Hoppenbrouwers <lemmings@demindiro.com>
2026-01-02 19:33:04 +0100

    remove PCI, ACPI code from kernel

--- x64/kernel.asm
+++ x64/kernel.asm
@@ -88,72 +88,11 @@ main:
 	sysretq
 	hlt
 
-	mov rax, cr3
-	mov dword [page_root], eax
-
-	call acpi_find_rsdp
-	test esi, esi
-	jz err_no_rsdp
-	log.ok msg_ok_rsdp
-
-	mov esi, [rsi + 16]
-	cmp dword [rsi], "RSDT"
-	jne err_no_rsdt
-	log.ok msg_ok_rsdt
-
-search_acpi_tables:
-	mov ecx, [rsi + 4]
-	sub ecx, 36
-	shr ecx, 2
-	add rsi, 36
-@@:	lodsd
-	mov eax, [eax]
-	cmp eax, "MCFG"
-	je .found_mcfg
-	cmp eax, "APIC" ; yes, "MADT" has "APIC" as signature
-	je .found_madt
-	loop @b
-	jmp .end
-.found_mcfg:
-	mov [acpi.mcfg], eax
-	log.ok msg_ok_mcfg
-	loop @b
-.found_madt:
-	mov [acpi.madt], eax
-	log.ok msg_ok_madt
-	loop @b
-.end:
-
-
-	call acpi.smp.init
-
-
-	ifne dword [acpi.mcfg], 0, @f
-	lea rsi, [msg_err_no_mcfg]
-	call printmsg.warn
-@@:
-
-	call pci_scan
-
-	hlt
-
-	mov edx, 0x604
-	mov eax, 0x2000
-	out dx, ax
-	hlt
-
-
-err_no_rsdp:
-	lea rsi, [msg_err_no_rsdp]
-	jmp panicmsg
-err_no_rsdt:
-	lea rsi, [msg_err_no_rsdt]
-	jmp panicmsg
 
 panicmsg:
-	mov ah, 4
-	call printmsg
-	panic
+    mov ah, 4
+    call printmsg
+    panic
 
 printmsg.warn:
 	mov ah, 6
@@ -204,26 +143,9 @@ macro msg name, s {
 }
 
 include "kernel/gdt.asm"
-include "kernel/acpi.asm"
-include "kernel/pci.asm"
 include "kernel/alloc.asm"
 include "kernel/paging.asm"
 include "kernel/syscall.asm"
-
-msg err_no_rsdp, "failed to find RSDP"
-msg err_no_rsdt, "RSDP does not point to RSDT"
-msg err_no_mcfg, "failed to find MCFG"
-msg ok_rsdp, "found RSDP"
-msg ok_rsdt, "found RSDT"
-msg ok_mcfg, "found MCFG"
-msg ok_madt, "found MADT"
-
-; this being 32-bits is very deliberate because
-; the APs need to be able to read it from real mode
-page_root: dd 0
-
-acpi.mcfg: dd 0
-acpi.madt: dd 0
 
 console.lock: db 0
 console.row: db 0
