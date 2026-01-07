412e5b87d67f8aaf8214d9b1a030d6bdc1b4f546
David Hoppenbrouwers <lemmings@demindiro.com>
2026-01-07 13:42:45 +0100

    uefi: COM1 on real HW (GA-AX370M-Gaming-3)

--- x64/uefi/boot.asm
+++ x64/uefi/boot.asm
@@ -63,35 +63,116 @@ EFI_SYSTEM_TABLE.ConOut           = 64 ; EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
 EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.Reset        = 0 ; EFI_TEXT_RESET
 EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString = 8 ; EFI_TEXT_STRING
 
-; rcx: EFI_HANDLE (of ourselves, just ignore)
-; rdx: EFI_SYSTEM_TABLE*
-start:
-	mov rax, [rdx + EFI_SYSTEM_TABLE.ConOut]
-	mov rbx, [rax + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
-
+macro print string {
 	; 12.4.3
 	; typedef EFI_STATUS (EFIAPI *EFI_TEXT_STRING) (
 	;   IN EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This
 	;   IN CHAR16 *String
 	; );
-	; I will _never_ forgive Microsoft for being so stubborn with (not even!) UTF-16
-	; instead of at least trying to use the actually sane UTF-8 standard
-	mov rcx, rax
-	lea rdx, [hello_uefi]
-	;call _break
-	call rbx
+	mov rdx, [rsp]
+	mov rcx, [rdx + EFI_SYSTEM_TABLE.ConOut]
+	lea rdx, [string]
+	sub rsp, 32
+	call qword [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
+	add rsp, 32
+}
 
+; rcx: EFI_HANDLE (of ourselves, just ignore)
+; rdx: EFI_SYSTEM_TABLE*
+start:
+	push rbp
+	mov rbp, rsp
+	push rcx
+	push rdx
+
+	print hello_uefi
+	print hello_uefi
+	print hello_uefi
+	print hello_uefi
+
+	cli
+
+
+
+COM1.IOBASE = 0x3f8
+COMx.rx          = 0
+COMx.tx          = 0
+COMx.intr_enable = 1
+COMx.intr_id     = 2
+COMx.baud_lo     = 0
+COMx.baud_hi     = 1
+COMx.ctrl_fifo   = 2
+COMx.ctrl_line   = 3
+COMx.ctrl_modem  = 4
+COMx.stat_line   = 5
+COMx.stat_modem  = 6
+COMx.scratch     = 7
+
+com1:
+macro outbi val {
+	mov al, val
+	out dx, al
+}
+.init:
+	print com1_init
+	mov dx, COM1.IOBASE + COMx.ctrl_line
+	outbi 1 shl 7
+	mov dx, COM1.IOBASE + COMx.baud_lo
+	outbi 1 ; 115200
+	inc dx  ; baud_hi
+	outbi 0
+	mov dx, COM1.IOBASE + COMx.ctrl_line
+	outbi (1 shl 3) or (1 shl 2) or (3 shl 0) ; 8E2
+	mov dx, COM1.IOBASE + COMx.intr_enable
+	outbi 0 ; don't bother with interrupts for now
+	mov dx, COM1.IOBASE + COMx.ctrl_fifo
+	outbi 7 ; clear buffers and enable
+	jmp .fin
+
+.test:
+	mov dx, COM1.IOBASE + COMx.ctrl_modem
+	outbi 011011b ; DTR, DTS, OUT2 (IRQ), loop
+	mov dx, COM1.IOBASE + COMx.tx
+	outbi 'X'
+	in al, dx
+	cmp al, 'X'
+	je .fin
+
+.fail:
+	mov rdx, [rsp]
+	mov rcx, [rdx + EFI_SYSTEM_TABLE.ConOut]
+	lea rdx, [com1_fail]
+	call qword [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
 @@:	hlt
 	jmp @b
 
-_break:
-	xor r15, r15
-@@:	test r15, r15
-	jz @b
-	ret
 
-hello_uefi: dw 'H', 'e', 'l', 'l', 'o', ' ', 'U', 'E', 'F', 'I', '!', 0
+.fin:
+	mov dx, COM1.IOBASE + COMx.ctrl_modem
+	outbi 01011b ; DTR, DTS, OUT2 (IRQ)
+	jmp .loop
 
+	print com1_ok
+	;jmp .halt
+
+.loop:
+	mov dx, COM1.IOBASE + COMx.tx
+	outbi 0
+	outbi 0
+	outbi 0
+	outbi 0
+	outbi 0x55
+	pause ; probably does nothing but...
+	jmp .loop
+
+.halt:
+@@:	hlt
+	jmp @b
+
+hello_uefi: dw 'H', 'e', 'l', 'l', 'o', ' ', 'U', 'E', 'F', 'I', '!', 13, 10, 0
+com1_init:  dw 'C', 'O', 'M', '1', ' ', 'i', 'n', 'i', 't', 13, 10, 0
+com1_fail:  dw 'C', 'O', 'M', '1', ' ', 'f', 'a', 'i', 'l', 'u', 'r', 'e', 13, 10, 0
+com1_ok:    dw 'C', 'O', 'M', '1', ' ', 'O', 'K', 13, 10, 0
 
 times ((-$) and 0xfff) db 0
 
