1110756d6c1c00318e2df2dd02e6caa2caf0738b
David Hoppenbrouwers <lemmings@demindiro.com>
2026-01-30 15:21:25 +0100

    Doc/arch/x64, x64/common: define LOG, HALT, IDENTIFIY syscalls

--- x64/common/syscall.asm
+++ x64/common/syscall.asm
@@ -23,13 +23,50 @@ syscall.init:
 syscall.entry64:
 	mov [syscall.scratch], rsp
 	mov rsp, _stack.end
-	irp x,rdx,rsi,rdi,rcx { push x }
+	irp x,rcx,rbx,rsi,rdi { push x }
 
+	dec eax
+	cmp eax, SYSCALL.MAX_SYSID
+	ja .bad_id
+	call qword [syscall.table + rax*8]
+
+	irp x,rdi,rsi,rbx,rcx { pop x }
+	mov rsp, [syscall.scratch]
+	sysretq
+
+.bad_id:
+@@:	hlt
+	jmp @b
+
+x = 0
+macro f id, routine {
+	x = x + 1
+	assert x = id
+	dq syscall.#routine
+}
+syscall.table:
+	f 1, log
+	f 2, halt
+	f 3, identify
+SYSCALL.MAX_SYSID = x
+purge f, x
+
+syscall.log:
+	push rdx
+	push rsi
 	mov ecx, edx
 	mov rdx, COM1.IOBASE
 	call comx.write
+	pop rax
+	pop rdx
+	sub rax, rsi
+	ret
 
-	irp x,rcx,rdi,rsi,rdx { pop x }
-	mov rsp, [syscall.scratch]
-	xor eax, eax
-	sysretq
+syscall.halt:
+	hlt
+	ret
+
+syscall.identify:
+	mov rax, "GeneSYS"
+	mov rdx, 0x20260130
+	ret
