77061aa9bbbd2ddfcf7bcef2b2b58fa97cf9a692
David Hoppenbrouwers <lemmings@demindiro.com>
2026-02-01 01:13:49 +0100

    x64/common/ioapic: disable PIT too

--- x64/common/ioapic.asm
+++ x64/common/ioapic.asm
@@ -10,8 +10,12 @@ ioapic.init:
 	mov al, 0xff
 	out 0x21, al
 	out 0xa1, al
-	; enable COM1
+	; make PIT shut up so QEMU doesn't spam trace
+	mov al, 0x30
+	out 0x43, al
+	; map IOAPIC
 	mov dword [paging.pt_mmio.ioapic], IOAPIC.PHYS_BASE + PAGE.D + PAGE.A + PAGE.RW + PAGE.G + PAGE.P
+	; enable COM1
 	mov dword [ioapic.addr], 0x10 + 2*4
 	mov dword [ioapic.data], 0x80f8
 	mov dword [ioapic.addr], 0x11 + 2*4
