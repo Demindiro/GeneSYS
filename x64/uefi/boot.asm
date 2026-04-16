14035d2509327ccb6516cf7465b6ffd2fd99f7d0
David Hoppenbrouwers <lemmings@demindiro.com>
2026-04-16 17:20:14 +0200

    x64/uefi: don't hardcode path to kernel
    Turns out %environment% variables can be used after all. I should have
    read the manual properly... Maybe even go through it entirely...

--- x64/uefi/boot.asm
+++ x64/uefi/boot.asm
@@ -633,8 +633,7 @@ include "../util/registers.asm"
 
 include "../common/kernel.inc"
 align 64
-; TODO avoid hardcoded path
-kernel: file "../../build/uefi/kernel.bin"
+kernel: file '%out%/kernel.bin'
 ; to simplify things, include libos along with the kernel :)
 times ((-$) and 63) int3   ; int3 as guard + align for faster memcpy on boot
 .libos: file CONFIG.LIBOS.PATH
