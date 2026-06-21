3e5b5575cb7575c78e7b99304d9d82526f4d3009
David Hoppenbrouwers <lemmings@demindiro.com>
2026-06-21 02:27:20 +0200

    move intel-vtd initialization code to iommu-intel.asm

--- x64/common/iommu-intel.asm
+++ x64/common/iommu-intel.asm
@@ -1,3 +1,45 @@
+; rsi: IOMMU registers base address
+intel_iommu.init:
+        ; map IOMMU registers
+	mov     rdi, paging.pt_mmio.iommu
+        or      rsi, PAGE.P + PAGE.RW + PAGE.G
+	mov     [rdi], rsi
+        ; check version (TODO actually check)
+        mov     eax, [iommu.intel.version]
+        ; allocate space for translation structures
+	call    _init.alloc_2m
+        push    rax     ; <0>
+	or      rax, PAGE.P + PAGE.PS + PAGE.RW + PAGE.G
+	mov     [paging.pd_misc.intel_iommu.translation_structures], rax
+        ; zero out translation structures
+        mov     ecx, (1 shl 21) / 8
+        mov     rdi, intel_iommu.translation_structures
+        xor     eax, eax
+        rep stosq
+        ; set root table in scalable mode
+        pop     rax     ; <0>
+        mov     rdx, rax
+        or      rax, (1 shl 10)
+        mov     [iommu.intel.root_table_addr], rax
+        ; link context table for bus 0
+        lea     rax, [rdx + (intel_iommu.context_table_0 - intel_iommu) + 1]
+        mov     [intel_iommu.root_address_table + (8*0)], rax
+        add     rax, 4096
+        mov     [intel_iommu.root_address_table + (8*1)], rax
+        ; link PASID tables
+        lea     rax, [rdx + (intel_iommu.pasid_table_0 - intel_iommu) + 1]
+        int3
+        mov     [intel_iommu.pasid_directory_0 + (8*0)], rax
+        add     rax, 4096
+        mov     [intel_iommu.pasid_directory_0 + (8*1)], rax
+        ; reload root table
+        mov     dword [iommu.intel.global_command], 1 shl 30
+        ; enable translation
+        mov     dword [iommu.intel.global_command], 2 shl 30
+        ret
+
+
+
 ; inputs:       rdx=segment:bcd,rsi=page physical root
 ; outputs:      rdx=segment:bcd(NOPE)
 ; clobbers:     rax,rcx,rdx,rdi,rsi
